d5fe9dafe4a892ffd246de0022b25f2167224353
berezkin88 <berezkin05@gmail.com>
2020-10-09 23:20:10 +0300

    firebase demo project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
+import { AngularFireDatabase } from 'angularfire2/database';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
-  title = 'firebase-demo';
+  courses$;
+  course$;
+  author$;
+
+  constructor(private db: AngularFireDatabase) {
+    this.courses$ = db.list('/courses').snapshotChanges();
+    this.course$ = db.object('/courses/-MJDugK3-phwmJtIFmnY').snapshotChanges()
+      .subscribe(course => console.log(course));
+    this.author$ = db.object('/authors/1').valueChanges();
+  }
+
+  add(course: HTMLInputElement) {
+    this.db.list('/courses').push({
+      name: course.value,
+      price: 150,
+      isLive: true,
+      sections: [
+        { title: 'Components' },
+        { title: 'Directives' },
+        { title: 'Templates' }
+      ]
+    });
+    course.value = '';
+  }
+
+  update(course) {
+    console.log(course);
+    this.db.object('/courses/' + course.key)
+      .set({
+        title: 'New Title',
+        isLive: true
+      });
+  }
+
+  delete(course) {
+    this.db.object('/courses/' + course.key)
+      .remove()
+      .then(x => console.log('Deleted'));
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC1fWmdajKoFhfoqXTpOCzLVqE3U6NK_ow",
+    authDomain: "fir-demo-355c7.firebaseapp.com",
+    databaseURL: "https://fir-demo-355c7.firebaseio.com",
+    projectId: "fir-demo-355c7",
+    storageBucket: "fir-demo-355c7.appspot.com",
+    messagingSenderId: "459054369572",
+    appId: "1:459054369572:web:9ead353744353eb37441b1"
+  }
 };
 
 /*
